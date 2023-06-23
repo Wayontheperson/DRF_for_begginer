24a5dd72a546b7823e3e9eb232c9fc9d1be7855a
Wayontherperson <ntcf652@gmail.com>
2023-06-23 19:06:53 +0900

    :white_check_mark: feat: Room Reviews pagination.
    For reduce DB's burden pagination option has been added.

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+
 from .models import Amenity, Room
 from users.serializers import TinyUserSerializer
 from categories.serializers import CategorySerializer
--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,9 @@
 from django.db import transaction
 
-from .models import Amenity, Room
 from . import serializers
+from .models import Amenity, Room
 from categories.models import Category
+from reviews.serializers import ReviewSerializer
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -170,3 +171,26 @@ class RoomsDetail(APIView):
             raise PermissionDenied
         room.delete()
         return Response(status=HTTP_204_NO_CONTENT)
+
+
+class RoomReviews(APIView):
+    def _get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        try:
+            page = request.query_params.get("page", 1)
+            page = int(page)
+        except ValueError:
+            page = 1
+        page_size = 3
+        start = (page - 1) * page_size
+        end = start + page_size
+        room = self._get_object(pk)
+        serializer = ReviewSerializer(room.reviews.all()[start:end],  # db에 가져온뒤 슬라이싱 하는게 아니라 슬라이싱 정보로 db heat
+                                      many=True)
+        return Response(serializer.data)
+
